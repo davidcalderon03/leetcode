ead2dfb7371083cd56cfd41a39b9eae9816be137
davidcalderon03 <davidcalderon03@hotmail.com>
2024-08-27 15:45:52 -0400

    0199

---
+++ 0199.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<vector<int>> arr;
+        int depth = 0;
+        helper(root, depth, arr);
+        vector<int> result;
+        for (vector<int> row : arr) {
+            result.push_back(row[row.size() - 1]);
+        }
+        return result;
+    }
+
+    void helper(TreeNode* node, int depth, vector<vector<int>>& arr) {
+        if (node == nullptr) return;
+        if (arr.size() <= depth) arr.push_back(vector<int>(0));
+        arr[depth].push_back(node->val);
+
+        helper(node->left, depth + 1, arr);
+        helper(node->right, depth + 1, arr);
+    }
+};
