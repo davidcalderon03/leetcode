ac180003098d45d180312287cf75de7676b3aa9a
dcalderon32 <dcalderon32@gatech.edu>
2024-09-05 21:35:57 -0400

    0030

--- 0030.cpp
+++ 0030.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string, int> word_map;
-        int word_len = words[0].length();
+        if (words.size() * words[0].length() > s.length()) return {};
+        map<string, int> mapping;
+        int length = words[0].length();
         vector<int> result;
         for(string s : words) {
-            word_map[s]++;
+            mapping[s]++;
         }
-        int max_index = s.length() - (words.size() * word_len);
-        for (int offset = 0; offset < word_len; ++offset) {
-            int start_index = offset;
-            while (start_index <= max_index) {
-                unordered_map<string, int> running_map;
-                for (int i = start_index; i < s.length() && i - start_index < words.size() * word_len; i+= word_len) {
-                    string substring = s.substr(i, word_len);
-                    if (word_map.find(substring) == word_map.end()) {
-                        break;
-                    } else {
-                        running_map[substring]++;
-                    }
-                    if (running_map == word_map) {
-                        result.push_back(start_index);
-                        break;
-                    }
+
+        for (int offset = 0; offset < length; ++offset) {
+            map<string, int> record;
+            int index = offset;
+            for (int i = 0; i < words.size(); ++i, index += length) {
+                string current_word = s.substr(index, length);
+                record[current_word]++;
+            }
+            while (index < s.length()) {
+                if (mapping == record) {
+                    result.push_back(index - words.size() * length);
                 }
-                start_index += word_len;
+                string left = s.substr(index - words.size() * length, length);
+                string right = s.substr(index, length);
+
+                record[left]--;
+                if (record[left] == 0) record.erase(left);
+                record[right]++;
+                index += length;
+            }
+            if (mapping == record) {
+                result.push_back(index - words.size() * length);
             }
         }
         return result;
     }
 };
+
+
+
+
+// Sliding window approach:
+// abcd efgh ijkl
+//  bcde fghi jklm 
+//   cdef ghij klmn
+//    defg hijk lmno
